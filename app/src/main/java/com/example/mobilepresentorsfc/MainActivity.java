db3aff38779f94f2c1d3ee32b6740692f4ef6a8b
MGalaktionov <mgalaktionov4@gmail.com>
2019-10-24 15:50:49 +0300

    added base menu wich contains file source selector

--- app/src/main/java/com/example/mobilepresentorsfc/MainActivity.java
+++ app/src/main/java/com/example/mobilepresentorsfc/MainActivity.java
@@ -5,10 +5,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private String code;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = getIntent();
+        code = intent.getStringExtra("code");
+        String[] fingerprints = VKUtil.getCertificateFingerprint(this, this.getPackageName());
+        System.out.println(Arrays.asList(fingerprints));
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+            }
+        } else {
+            // Permission has already been granted
+        }
+    }
+
+    public void onVKClick(View view) {
+        Intent vkIntent = new Intent(this, VKLoginActivity.class);
+        vkIntent.putExtra("code", code);
+        startActivity(vkIntent);
+    }
+
+    public void onGoogleClick(View view) {
+        Intent googleDriveIntent = new Intent(this, GoogleDriveLoginActivity.class);
+        googleDriveIntent.putExtra("code", code);
+        startActivity(googleDriveIntent);
     }
 }
