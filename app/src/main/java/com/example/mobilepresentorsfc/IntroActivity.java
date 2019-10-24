438e5bab3ae4f1d28fe17709a0ca4fa0695ad15f
MGalaktionov <mgalaktionov4@gmail.com>
2019-10-24 15:43:48 +0300

    fix imports

--- app/src/main/java/com/example/mobilepresentorsfc/IntroActivity.java
+++ app/src/main/java/com/example/mobilepresentorsfc/IntroActivity.java
@@ -1,8 +1,9 @@
 package com.example.mobilepresentorsfc;
 
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 public class IntroActivity extends AppCompatActivity {
     private ViewPager viewPager;
